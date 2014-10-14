6fb0c86e038a91133584f96c9f6f069476dbe724
Samuel Volk <samivolk16@hotmail.com>
2014-10-13 22:29:02 -0400

    Added documentation

--- lab4/partA/partA.ino
+++ lab4/partA/partA.ino
@@ -1,9 +1,32 @@
-byte ledPin[] = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13}; // Create array for LED pins
-int ledDelay;   // delay between changes
-int direction = 1;
-int currentLED = 0;
-unsigned long changeTime;
-int potPin = 2; // select the input pin for the potentiometer
+/*
+    Lab 4 - Part A
+    Potentiometer + LED Blinking speed control
+
+    Samuel Volk, 07/10/2014
+
+    This sketch makes 10 LEDs blink one way but the speed is controlled by a
+    potentiometer.
+
+
+    Debugging and Verification
+    - copied 4-b from powerpoint but most of the whitespace messed up, fixed
+    - needed to sop LEDs from cycling back so commented out both reversing
+      lines, LEDs went one way then randomly blinked (overflow or something?)
+    - uncommented on reversing line, LEDs blinked one way then the other then
+      stopped
+    - changed single remaining reversing if() statement to reset current LED
+      back to 0, works
+*/
+
+
+// Global Variables
+byte ledPin[] = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13};// Create array for LED pins
+int ledDelay;               // delay between changes
+int direction = 1;          // holds step + direction
+int currentLED = 0;         // holds current LED
+unsigned long changeTime;   // time since delay started related to the
+                            // processor runtime
+int potPin = 2;             // select the input pin for the potentiometer
 
 
 void setup()
@@ -18,7 +41,8 @@ void loop()
 {
     ledDelay = analogRead(potPin); // read the value from the pot
 
-    if ((millis() - changeTime) > ledDelay) // if it has been ledDelay ms since last change
+    if ((millis() - changeTime) > ledDelay) // has it been longer than ledDelay
+                                            // since last blink
     {
         for (int x=0; x<10; x++)
         {
@@ -31,6 +55,7 @@ void loop()
         // return to the start if the end is reached
         if (currentLED == 10) currentLED = 0;
 
+        // reset time since last blink
         changeTime = millis();
-    } //close if
-} //close loop
+    }
+}
--- lab4/partB/partB.ino
+++ lab4/partB/partB.ino
@@ -1,12 +1,29 @@
 /*
-crap i have to write about because it didn't work:
+    Lab 4 - Part B
+    SOS LED
+
+    Samuel Volk, 07/10/2014
+
+    This sketch just blinks an LED with morse code of "SOS". It blinks it 5
+    times then waits 1 second then repeats. Dits are 1 unit, dahs are 3, there
+    is a 7 unit wait between words ("SOS" is a word), 1 unit in between
+    letters.
 
 
+    Debugging and Verification:
+    - moved repetitive blinking code to blink() function
+    - wrote wait() function
+    - modified blink() function to include delays so delay() is now not needed
+    - blink rate a little too slow, was 250 now set to 200
 */
 
-int led = 8;
-int tconst = 200;
+// Global variables
+int led = 8;        // led pin #
+int tconst = 200;   // time constant value
 
+
+// blink function to blink led with an on time and an offtime (both multiplied
+// against the time constant)
 void blink(int ontime, int offtime)
 {
     digitalWrite(led, HIGH);
@@ -18,13 +35,13 @@ void blink(int ontime, int offtime)
 
 void setup()
 {
-    pinMode(led, OUTPUT);
-    // all our pins are ready!
-    // let's rock and roll ladies
+    pinMode(led, OUTPUT);   // all our pins are ready!
+                            // let's rock and roll ladies
 }
 
 void loop()
 {
+     // cycle 5 times through SOS
     for (int i = 0; i < 5; i++)
     {
         // S
@@ -38,7 +55,6 @@ void loop()
         blink(3, 1);
         blink(3, 1);
         blink(3, 3);    // 3 time constants because end of letter
-
 
         // S
         // ...
